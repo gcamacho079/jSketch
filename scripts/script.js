469bf2aaa7293755e8e84f4628917b8c17c9380b
Guadalupe Camacho <gcamacho079@gmail.com>
2016-09-22 09:30:05 -0700

    Add new grid function

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,61 @@
-$(document).ready(function() {
-  var squareBuilder = "<div class='gridSquare'></div>";
-  for (i = 0; i < 256; i++) {
-    $(".gridArea").append(squareBuilder);
-  }
-  $("div div:nth-child(16n)").after("<br>");
+var etchASketch = {
 
-  $(document).on('mouseenter', '.gridSquare', function() {
+  /**************************
+  DRAWS INITIAL 16x16 GRID
+  ***************************/
+  onReady: function() {
+    var squareBuilder = "<div class='gridSquare'></div>";
+    for (i = 0; i < 256; i++) {
+      $(".gridArea").append(squareBuilder);
+    }
+    $("div div:nth-child(16n)").after("<br>");
+
+    $(".gridSquare").mouseenter(etchASketch.penTool);
+    $("#new-grid").click(etchASketch.drawNewGrid);
+  },
+
+  /**************************
+  DRAWING FUNCTION
+  ***************************/
+  penTool: function(mouseenter) {
     $(this).css('background-color', 'blue');
-  });
 
-  $(document).on('click', '#new-grid', function() {
-    $('.gridSquare').remove();
-  });
+  },
 
-});
+  /**************************
+  NEW GRID FUNCTION
+  ***************************/
+  drawNewGrid: function() {
+    var squaresPerSide = prompt("How many squares wide should this grid be? Enter a number between 1-690:");
+    $('.gridArea').empty();
+
+    /**** Calculates new length and width ****/
+
+    var pixelsPerBox = (690/squaresPerSide);
+    pixelsPerBox = pixelsPerBox;
+
+    /**** Assigns new CSS properties (NOT WORKING)****/
+
+    console.log(pixelsPerBox);
+    $allBoxes = $(".gridSquare");
+    $allBoxes.height(pixelsPerBox);
+    $allBoxes.width(pixelsPerBox);
+    console.log($allBoxes.width());
 
 
-/*
+    /**** Builds new grid ****/
 
-var squaresPerSide;
-var pixelsPerBox = 960/squaresPerSide;
-var lwInPixels = pixelsPerBox + "px";
-$('gridSquare.').width('lwInPixels') */
+    var squareBuilder = "<div class='gridSquare'></div>";
+    for (i = 0; i < (squaresPerSide * squaresPerSide); i++) {
+      $(".gridArea").append(squareBuilder);
+    }
+    var $linebreakConstructor = $('div div:nth-child(' + squaresPerSide + 'n)');
+    $linebreakConstructor.after("<br>");
+
+    $(".gridSquare").mouseenter(etchASketch.penTool);
+
+  }
+};
+
+
+$(document).ready(etchASketch.onReady);
